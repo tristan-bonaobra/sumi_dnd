4ca1c52adc0a36ce4208d4088878da947e1d6318
Tristan Bonaobra <316808543+tristan-bonaobra@users.noreply.github.com>
2026-08-16 23:59:34 +0800

    feat: add parent-subclass relationship between classes
    Replace the old, perk-like system of subclasses.

--- 01_schema.sql
+++ 01_schema.sql
@@ -1,17 +1,36 @@
 CREATE TABLE class (
 	id SERIAL PRIMARY KEY,
-	name TEXT
+	name TEXT UNIQUE,
+	parent_id INT REFERENCES class(id) ON DELETE CASCADE
 );
 
 CREATE TABLE stat (
 	id SERIAL PRIMARY KEY,
-	long TEXT,
-	short CHAR(3)
+	long TEXT UNIQUE,
+	short CHAR(3) UNIQUE
 );
 
 CREATE TABLE stat_type (
 	id SERIAL PRIMARY KEY,
-	name TEXT
+	name TEXT UNIQUE
+);
+
+CREATE TABLE bonus_att (
+	id SERIAL PRIMARY KEY,
+	name TEXT UNIQUE
+);
+
+CREATE TABLE ability (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	def TEXT,
+	cd INT
+);
+
+CREATE TABLE passive (
+	id SERIAL PRIMARY KEY,
+	name TEXT,
+	def TEXT
 );
 
 CREATE TABLE class_stat (
@@ -22,23 +41,11 @@ CREATE TABLE class_stat (
 	UNIQUE (class_id, stat_id, type_id)
 );
 
-CREATE TABLE bonus_att (
-	id SERIAL PRIMARY KEY,
-	name TEXT UNIQUE
-);
-
 CREATE TABLE class_bonus (
 	id SERIAL PRIMARY KEY,
 	class_id INT REFERENCES class(id) ON DELETE CASCADE,
 	bonus_id INT REFERENCES bonus_att(id) ON DELETE CASCADE,
 	UNIQUE (class_id, bonus_id)
-);
-
-CREATE TABLE ability (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	def TEXT,
-	cd INT
 );
 
 CREATE TABLE class_ability (
@@ -47,28 +54,10 @@ CREATE TABLE class_ability (
 	ability_id INT REFERENCES ability(id) ON DELETE CASCADE
 );
 
-CREATE TABLE passive (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	def TEXT
-);
-
 CREATE TABLE class_passive (
 	id SERIAL PRIMARY KEY,
 	class_id INT REFERENCES class(id) ON DELETE CASCADE,
 	passive_id INT REFERENCES passive(id) ON DELETE CASCADE
-);
-
-CREATE TABLE subclass (
-	id SERIAL PRIMARY KEY,
-	name TEXT,
-	def TEXT
-);
-
-CREATE TABLE class_subclass (
-	id SERIAL PRIMARY KEY,
-	class_id INT REFERENCES class(id) ON DELETE CASCADE,
-	subclass_id INT REFERENCES subclass(id) ON DELETE CASCADE
 );
 
 CREATE TABLE player (
@@ -76,49 +65,65 @@ CREATE TABLE player (
 	name TEXT
 );
 
-create TABLE rpchar (
+CREATE TABLE rpchar (
 	id SERIAL PRIMARY KEY,
 	name TEXT,
 	player_id INT REFERENCES player(id)
 );
 
+CREATE VIEW v_class_child AS SELECT -- For each parent
+	class.id AS parent_id,
+	string_agg(child.name, E'\n' ORDER BY child.name) AS list
+FROM class
+LEFT JOIN class AS child ON child.parent_id = class.id
+GROUP BY class.id;
+
 CREATE VIEW v_class_stat AS SELECT
-	class_stat.class_id as class_id,
-	string_agg(stat.short || ' ' || stat_type.name, E'\n') as list
+	class_stat.class_id AS class_id,
+	string_agg(stat.short || ' ' || stat_type.name, E'\n' ORDER BY class_stat.type_id, stat.id) AS list
 FROM class_stat
 LEFT JOIN stat ON stat.id = class_stat.stat_id
 LEFT JOIN stat_type ON stat_type.id = class_stat.type_id
 GROUP BY class_stat.class_id;
 
 CREATE VIEW v_class_bonus AS SELECT
-	class_bonus.class_id as class_id,
-	string_agg(bonus_att.id || ' ' || bonus_att.name, E'\n') as list
+	class_bonus.class_id AS class_id,
+	string_agg(bonus_att.name, E'\n' ORDER BY bonus_att.name) AS list
 FROM class_bonus
 LEFT JOIN bonus_att ON bonus_att.id = class_bonus.bonus_id
 GROUP BY class_bonus.class_id;
 
 CREATE VIEW v_class_ability AS SELECT
-	class_ability.class_id as class_id,
-	string_agg(ability.id || ' ' || ability.name, E'\n') as list
+	class_ability.class_id AS class_id,
+	string_agg(ability.name, E'\n' ORDER BY ability.name) AS list
 FROM class_ability
 LEFT JOIN ability ON ability.id = class_ability.ability_id
 GROUP BY class_ability.class_id;
 
 CREATE VIEW v_class_passive AS SELECT
-	class_passive.class_id as class_id,
-	string_agg(passive.id || ' ' || passive.name, E'\n') as list
+	class_passive.class_id AS class_id,
+	string_agg(passive.name, E'\n' ORDER BY passive.name) AS list
 FROM class_passive
 LEFT JOIN passive ON passive.id = class_passive.passive_id
 GROUP BY class_passive.class_id;
 
 CREATE VIEW class_wiki AS SELECT
-	class.id || ' ' || class.name as class,
-	v_class_stat.list as stats,
-	v_class_bonus.list as bonuses,
-	v_class_ability.list as abilities,
-	v_class_passive.list as passives
+	class.id as id,
+	class.name AS name,
+	COALESCE(v_class_child.list, '-') AS subclasses,
+	COALESCE(parent.name, '-') AS subclass_of,
+	v_class_stat.list AS stats,
+	v_class_bonus.list AS bonuses,
+	v_class_ability.list AS abilities,
+	v_class_passive.list AS passives
 FROM class
-LEFT JOIN v_class_stat on v_class_stat.class_id = class.id
-LEFT JOIN v_class_bonus on v_class_bonus.class_id = class.id
-LEFT JOIN v_class_ability on v_class_ability.class_id = class.id
-LEFT JOIN v_class_passive on v_class_passive.class_id = class.id;
+LEFT JOIN v_class_child ON v_class_child.parent_id = class.id
+LEFT JOIN class AS parent ON parent.id = class.parent_id
+LEFT JOIN v_class_stat ON v_class_stat.class_id = class.id
+LEFT JOIN v_class_bonus ON v_class_bonus.class_id = class.id
+LEFT JOIN v_class_ability ON v_class_ability.class_id = class.id
+LEFT JOIN v_class_passive ON v_class_passive.class_id = class.id
+ORDER BY
+	COALESCE(class.parent_id, class.id), -- Default parent_id to own id to meet family
+	class.parent_id NULLS FIRST, -- Start with parents
+	class.name; -- Sort among siblings
--- 02_seed.sql
+++ 02_seed.sql
@@ -1,3 +1,4 @@
+-- Stat
 INSERT INTO stat(long, short) VALUES('Strength', 'STR');
 INSERT INTO stat(long, short) VALUES('Dexterity', 'DEX');
 INSERT INTO stat(long, short) VALUES('Constitution', 'CON');
@@ -5,14 +6,17 @@ INSERT INTO stat(long, short) VALUES('Intelligence', 'INT');
 INSERT INTO stat(long, short) VALUES('Wisdom', 'WIS');
 INSERT INTO stat(long, short) VALUES('Charisma', 'CHA');
 
+-- Stat type
 INSERT INTO stat_type(name) VALUES('Main');
 INSERT INTO stat_type(name) VALUES('Sub');
 
+-- Bonus attribute
 INSERT INTO bonus_att(name) VALUES('Physical Damage Type');
 INSERT INTO bonus_att(name) VALUES('Durability Buffer');
 INSERT INTO bonus_att(name) VALUES('Melee');
 INSERT INTO bonus_att(name) VALUES('Damage Stacking');
 
+-- Ability
 INSERT INTO ability(name, def, cd) VALUES ('Slash', 'Deal [STR Base] physical damage to a single target.', 1);
 INSERT INTO ability(name, def, cd) VALUES ('Defend', 'Negate [CON Base] Physical damage taken for 1 round.', 1);
 INSERT INTO ability(name, def, cd) VALUES ('Concentrate', 'Gain +1 physical damage bonus for 6 rounds.', 6);
@@ -20,14 +24,11 @@ INSERT INTO ability(name, def, cd) VALUES ('Rage', 'Gain [Rage] for 2 rounds.', 
 INSERT INTO ability(name, def, cd) VALUES ('Axe Throw', 'Deal [STR base] physical damage to a single target.', 1);
 INSERT INTO ability(name, def, cd) VALUES ('Axe Swing', 'Deal [STR base] physical to all enemies.', 4);
 
+-- Passive
 INSERT INTO passive(name, def) VALUES ('Strength Buffer', '+1 Strength.');
 INSERT INTO passive(name, def) VALUES ('Broken Blade', 'At below 50% HP, gain +1 Strength.');
 INSERT INTO passive(name, def) VALUES ('Mediate', 'Taking damage with [Rage] grants a stack of [Mediate].');
 INSERT INTO passive(name, def) VALUES ('Frustration', 'Increase crit damage by 50%.');
-
-INSERT INTO subclass(name, def) VALUES ('Corrupted Knight', 'Sacrifice health for high damage.');
-INSERT INTO subclass(name, def) VALUES ('Blade Dancer', 'Ability combos and follow-ups.');
-INSERT INTO subclass(name, def) VALUES ('Wolf Teeth', 'Debuffs and damage over time.');
 
 INSERT INTO class(name) VALUES('Warrior');
 INSERT INTO class_stat(class_id, stat_id, type_id) VALUES(1, 1, 1);
@@ -42,11 +43,8 @@ INSERT INTO class_ability(class_id, ability_id) VALUES (1, 2);
 INSERT INTO class_ability(class_id, ability_id) VALUES (1, 3);
 INSERT INTO class_passive(class_id, passive_id) VALUES (1, 1);
 INSERT INTO class_passive(class_id, passive_id) VALUES (1, 2);
-INSERT INTO class_subclass(class_id, subclass_id) VALUES(1, 1);
-INSERT INTO class_subclass(class_id, subclass_id) VALUES(1, 2);
-INSERT INTO class_subclass(class_id, subclass_id) VALUES(1, 3);
 
-INSERT INTO class(name) VALUES('Barbarian');
+INSERT INTO class(name, parent_id) VALUES('Barbarian', 1);
 INSERT INTO class_stat(class_id, stat_id, type_id) VALUES(2, 1, 1);
 INSERT INTO class_stat(class_id, stat_id, type_id) VALUES(2, 2, 1);
 INSERT INTO class_stat(class_id, stat_id, type_id) VALUES(2, 3, 2);
