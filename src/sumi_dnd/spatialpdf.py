43dce8e8190d8c7669183fd7c439e4170fdf802f
Tristan Bonaobra <316808543+tristan-bonaobra@users.noreply.github.com>
2026-08-24 21:13:33 +0800

    chore: cleanup leftover code from previous wipe
    Remove unused glossary entries and dependencies.

--- src/sumi_dnd/spatialpdf.py
+++ src/sumi_dnd/spatialpdf.py
@@ -1,10 +1,7 @@
 # Glossary
 # char          pdfplumber character dict
-# fgroup        format group; chars grouped based on size and non-stroking color; to group into an fgroup
-# linegroup     chars grouped by top; to group into a linegroup
 
 import pdfplumber
-import math
 from pathlib import Path
 
 repo_dir = Path(__file__).resolve().parents[2]
