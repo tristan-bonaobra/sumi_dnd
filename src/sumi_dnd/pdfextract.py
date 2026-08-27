e48e80c5a7f1001fff5e626566f8626881cc9ced
Tristan Bonaobra <316808543+tristan-bonaobra@users.noreply.github.com>
2026-08-27 16:38:02 +0800

    refactor: check page.images for cards
    Correct the previous approach of checking rects.
    
    Also, force an order for extracting from stat cards, as markers often overlap.
    
    TODO: Extract from skill cards, list format assumptions in README.txt

--- src/sumi_dnd/pdfextract.py
+++ src/sumi_dnd/pdfextract.py
@@ -1,5 +1,4 @@
 # Glossary
-# crect         class rectangle, could be a stat card, could be a skill card
 # nsc           non-stroking color
 # cformat       char format
 # cmformat      comment format
@@ -11,19 +10,17 @@ from pathlib import Path
 from collections import defaultdict
 
 # Measurements based on Blade Dancer 22 Aug 2026
-EXPECTED_CRECT_WIDTH = 1439.99994
-EXPECTED_CRECT_HEIGHT = 809.9999662499999
-expected_crect_ratio = EXPECTED_CRECT_WIDTH / EXPECTED_CRECT_HEIGHT
-CRECT_BUFFER_PCT = 0.05
-CRECT_VSPLIT_OFFSET = 5 # Split some distance to the right of where Passive starts
-CRECT_HEADER_CUTOFF = 52.54079888160038
-CRECT_HEADER_BUFFER = 5 # Start the actual cutoff some distance below CRECT_HEADER_CUTOFF
+TEMPLATE_CARD_WIDTH = 742.215545074351
+TEMPLATE_CARD_HEIGHT = 521.997746250093
+expected_card_ratio = TEMPLATE_CARD_WIDTH / TEMPLATE_CARD_HEIGHT
+CARD_RATIO_BUFFER_PCT = 0.025
+CARD_VSPLIT_OFFSET = 5 # Split some distance to the right of where Passive starts
 
 # List all possible forms of the same functional marker
-MARKERS_MAIN_STATS = ["Main Stat"]
-MARKERS_SUB_STATS = ["Simplified", "Sub Stat"]
-MARKERS_BONUS_ATTS = ["Bonus"]
-MARKERS_SKILL_CARD = ["Class abilities"]
+MARKERS_MAIN_STATS = ["Main"]
+MARKERS_SUB_STATS = ["Simplified", "Sub"]
+MARKERS_BONUS_ATTS = ["Simplified", "Bonus"]
+MARKERS_SKILL_CARD = ["abilities"]
 MARKERS_ITALICS = ["italic", "oblique"]
 MARKERS_BOLD = ["bold", "bd", "heavy", "thick", "blk", "black", "medi"]
 
@@ -31,12 +28,16 @@ MARKERS_BOLD = ["bold", "bd", "heavy", "thick", "blk", "black", "medi"]
 CMFORMAT_NSC = (0.5725, 0.5725, 0.5725) # This assumes DeviceRGB.
 CMFORMAT_NSC_BUFFER = 0.075
 
+# A default is set for these values by pdfplumber.
+EXTRACT_TEXT_X_TOLERANCE = 0
+EXTRACT_TEXT_Y_TOLERANCE = 0
+
 # For the left card we'll go by markers in the text itself.
 # For the right card we'll assume only that subheaders are bold, and uniquely so.
 
 repo_dir = Path(__file__).resolve().parents[2]
 dm_dir = repo_dir / "dm"
-pdf_path = dm_dir / "class_knight.pdf"
+pdf_path = dm_dir / "class_warrior.pdf"
 
 #-------------------------------------------------------------------------------------------------+
 #   META ANALYSIS
@@ -76,14 +77,14 @@ def confirm_any_marker_in_text(markers, text, case_sensitive=False):
 #   FORMAT MATCHING
 #-------------------------------------------------------------------------------------------------+
 
-def confirm_rect_is_crect(rect):
-    rect_width = rect["width"]
-    rect_height = rect["height"]
-    rect_ratio = rect_width / rect_height
-    ratio_diff = abs(rect_ratio - expected_crect_ratio)
-    within_buffer = ratio_diff <= (expected_crect_ratio * CRECT_BUFFER_PCT)
-    is_crect = within_buffer
-    return is_crect
+def confirm_image_is_card(image):
+    image_width = image["width"]
+    image_height = image["height"]
+    image_ratio = image_width / image_height
+    ratio_diff = abs(image_ratio - expected_card_ratio)
+    within_buffer = ratio_diff <= (expected_card_ratio * CARD_RATIO_BUFFER_PCT)
+    is_card = within_buffer
+    return is_card
 
 def confirm_char_matches_cmformat(char):
     color_dist = math.dist(char["non_stroking_color"], CMFORMAT_NSC)
@@ -96,14 +97,26 @@ def confirm_char_matches_cmformat(char):
 #   CROP TO VIEW
 #-------------------------------------------------------------------------------------------------+
 
-def crop_to_rect(page, rect):
-    box = (rect["x0"], rect["top"], rect["x1"], rect["bottom"])
+def clamp(x, v_min, v_max):
+    return max(v_min, min(x, v_max))
+
+def clamp_box_to_page(box, page):
+    old_x0, old_y0, old_x1, old_y1 = box
+    new_x0 = clamp(old_x0, 0, page.width)
+    new_y0 = clamp(old_y0, 0, page.height)
+    new_x1 = clamp(old_x1, 0, page.width)
+    new_y1 = clamp(old_y1, 0, page.height)
+    return (new_x0, new_y0, new_x1, new_y1)
+
+def crop_to_image(page, image):
+    box = (image["x0"], image["top"], image["x1"], image["bottom"])
+    box = clamp_box_to_page(box, page)
     view = page.crop(box)
     return view
 
-def crop_to_rect_vsplit(page, rect, split_x): # Can't get CroppedPage bounds it seems
-    left_box = (rect["x0"], rect["top"], split_x, rect["bottom"])
-    right_box = (split_x, rect["top"], rect["x1"], rect["bottom"])
+def crop_to_image_vsplit(page, image, split_x): # Can't get CroppedPage bounds it seems
+    left_box = (image["x0"], image["top"], split_x, image["bottom"])
+    right_box = (split_x, image["top"], image["x1"], image["bottom"])
     left_view = page.crop(left_box)
     right_view = page.crop(right_box)
     return left_view, right_view
@@ -133,10 +146,10 @@ def filter_keep_subheaders(object): # If it's bold, it's a subheader
 #   EXTRACTION
 #-------------------------------------------------------------------------------------------------+
 
-def extract_crects_from_page(page):
-    rects = page.rects
-    crects = [rect for rect in rects if confirm_rect_is_crect(rect)]
-    return crects
+def extract_cards_from_page(page):
+    images = page.images
+    cards = [image for image in images if confirm_image_is_card(image)]
+    return cards
 
 def extract_subheader_markers_from_view(view):
     filtered_page = view.filter(filter_keep_subheaders)
@@ -152,28 +165,29 @@ def extract_stats_from_text(text):
         "bonus_atts": []
     }
     new_rpgclass_name = None
-    mode = "init"
+    step = 0 # 1:Init, 2:Main, 3:Sub, 4:Bonus
+    # We're forcing an order here because sometimes "Simplified" can mean either sub stats or bonus attributes.
     for key, line in enumerate(text.split("\n")):
         stripped_line = line.rstrip()
         stat = stripped_line.upper()
         extracting_main = confirm_any_marker_in_text(MARKERS_MAIN_STATS, line)
-        extracting_sub = confirm_any_marker_in_text(MARKERS_SUB_STATS, line)
         extracting_bonus_atts = confirm_any_marker_in_text(MARKERS_BONUS_ATTS, line)
-        if extracting_main:
-            mode = "main"
+        extracting_sub = confirm_any_marker_in_text(MARKERS_SUB_STATS, line)
+        if extracting_main and (step == 0):
+            step = 1
             continue # Skip the subheader
-        elif extracting_sub:
-            mode = "sub"
+        elif extracting_sub and (step == 1):
+            step = 2
             continue
-        elif extracting_bonus_atts:
-            mode = "bonus_atts"
+        elif extracting_bonus_atts and (step == 2):
+            step = 3
             continue
-        if mode == "init":
+        if step == 0:
             if key == 0: new_rpgclass_name = stripped_line
             else: new_rpgclass["def"] += line # From here on out assume rpgclass exists
-        elif mode == "main": new_rpgclass["main"].append(stat)
-        elif mode == "sub": new_rpgclass["sub"].append(stat)
-        elif mode == "bonus_atts": new_rpgclass["bonus_atts"].append(line)
+        elif step == 1: new_rpgclass["main"].append(stat)
+        elif step == 2: new_rpgclass["sub"].append(stat)
+        elif step == 3: new_rpgclass["bonus_atts"].append(line)
     new_rpgclasses = {}
     new_rpgclasses[new_rpgclass_name] = new_rpgclass
     return new_rpgclasses
@@ -185,16 +199,10 @@ def extract_stats_from_text(text):
 with pdfplumber.open(pdf_path) as pdf:
     all_rpgclasses = defaultdict(dict)
     for page in pdf.pages:
-        hi = 0
-        for rect in page.images:
-            print("IsCrect:", confirm_rect_is_crect(rect), "Width:", round(rect["width"], 1), "Height:", round(rect["height"], 1), "Ratio:", round(rect["width"] / rect["height"], 1), f"\n")
-            if round(rect["width"] / rect["height"], 1) == 1.4:
-                hi += 1
-        print(hi)
-        for crect in extract_crects_from_page(page):
-            current_view = crop_to_rect(page, crect)
+        for card in extract_cards_from_page(page):
+            current_view = crop_to_image(page, card)
             current_view = current_view.filter(filter_remove_comments)
-            current_text = current_view.extract_text()
+            current_text = current_view.extract_text(x_tolerance=EXTRACT_TEXT_X_TOLERANCE, y_tolerance=EXTRACT_TEXT_Y_TOLERANCE)
             is_skill_card = confirm_any_marker_in_text(MARKERS_SKILL_CARD, current_text)
             is_stat_card = not is_skill_card
             new_rpgclasses = {}
@@ -202,4 +210,5 @@ with pdfplumber.open(pdf_path) as pdf:
                 new_rpgclasses = extract_stats_from_text(current_text)
             for new_rpgclass_name, new_rpgclass in new_rpgclasses.items():
                 all_rpgclasses[new_rpgclass_name] |= new_rpgclass
-    print(json.dumps(all_rpgclasses, indent=4))
+    with open(r"C:\Users\tjames\Desktop\extracted_classes.json", "w", encoding="utf-8") as f:
+        json.dump(all_rpgclasses, f, indent=4, ensure_ascii=False)
