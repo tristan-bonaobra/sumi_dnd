b11236cf5c78b544fc27ee108a75876389d6c8f5
Tristan Bonaobra <316808543+tristan-bonaobra@users.noreply.github.com>
2026-09-07 18:34:53 +0800

    feat: add schema reinitialization
    init_schema

--- src/sumi_dnd/db_init.py
+++ src/sumi_dnd/db_init.py
@@ -1,12 +1,16 @@
 from sumi_dnd import db_engine
 from sqlalchemy import text
+from pathlib import Path
 
 engine = db_engine.get_engine()
+script_dir = Path(__file__).parent
+schema_source_path = script_dir / "schema.sql"
 
 def reinitialize_database():
     print("Pushing the big red button")
     nuke_schema()
     create_readonly_role()
+    init_schema()
 
 def nuke_schema():
     with engine.begin() as conn:
@@ -24,5 +28,10 @@ def create_readonly_role():
             conn.rollback()
         conn.execute(text("GRANT pg_read_all_data TO readonly;"))
         conn.commit()
+
+def init_schema():
+    with engine.begin() as conn:
+        with open(schema_source_path, "r", encoding="utf-8") as file:
+            conn.execute(text(file.read()))
 
 reinitialize_database()
