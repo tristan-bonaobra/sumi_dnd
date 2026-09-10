430081f9c5b5af839193ce72b8ee389063829ed0
Tristan Bonaobra <316808543+tristan-bonaobra@users.noreply.github.com>
2026-09-10 17:35:17 +0800

    style: move one print message

--- src/sumi_dnd/db/reinit.py
+++ src/sumi_dnd/db/reinit.py
@@ -7,12 +7,12 @@ sumi_dnd_dir = Path(__file__).resolve().parents[1]
 schema_source_path = sumi_dnd_dir / "sql" / "schema.sql"
 
 def reinitialize_database():
-    print("Pushing the big red button")
     nuke_schema()
     create_readonly_role()
     init_schema()
 
 def nuke_schema():
+    print("Pushing the big red button")
     with engine.begin() as conn:
         conn.execute(text("""
             DROP SCHEMA IF EXISTS public CASCADE;
