3934368cbcbdf38aeff76753b7c4ec7196605b4e
Tristan Bonaobra <316808543+tristan-bonaobra@users.noreply.github.com>
2026-09-17 01:16:02 +0800

    refactor: formalize test.py into ingest.py

--- src/sumi_dnd/ingest.py
+++ src/sumi_dnd/ingest.py
@@ -5,9 +5,10 @@ from sumi_dnd.tag import seed_tags, insert_tags_for_skills, nuke_database_and_ge
 INSERT_FILES = ["class_knight.pdf", "class_warrior.pdf"]
 SKILL_TYPES = ["ability", "passive"]
 
-nuke_database_and_generate_prompts()
-reinitialize_database()
-for file_name in INSERT_FILES: insert_pdf(file_name)
-for skill_type in SKILL_TYPES: insert_keywords_for_skill_type(skill_type)
-seed_tags()
-for skill_type in SKILL_TYPES: insert_tags_for_skills(skill_type)
+def ingest():
+    nuke_database_and_generate_prompts()
+    reinitialize_database()
+    for file_name in INSERT_FILES: insert_pdf(file_name)
+    for skill_type in SKILL_TYPES: insert_keywords_for_skill_type(skill_type)
+    seed_tags()
+    for skill_type in SKILL_TYPES: insert_tags_for_skills(skill_type)
